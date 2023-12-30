fd84853e7cc4bbfd3344e8b429bf031df62318c5
Egor <www.makeevegor1996@mail.ru>
2023-12-30 14:52:31 +0300

    initial commit

---
+++ book-store-admin.cpp
@@ -0,0 +1,12 @@
+﻿// book-store-admin.cpp: определяет точку входа для приложения.
+//
+
+#include "book-store-admin.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
